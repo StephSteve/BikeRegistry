c3102ec050eeec3628a035f006a21a7c31b4b74b
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-09-23 23:04:01 -0400

    rolled back all Auth0 config

--- bike/bike/src/main/java/com/globomatics/bike/configuration/WebConfig.java
+++ bike/bike/src/main/java/com/globomatics/bike/configuration/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/bikes")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .maxAge(3600L)
                 .allowedHeaders("AUTHORIZATION")
                 .allowCredentials(true);
--- bike/bike/src/main/java/com/globomatics/bike/models/Bike.java
+++ bike/bike/src/main/java/com/globomatics/bike/models/Bike.java
@@ -2,14 +2,12 @@ package com.globomatics.bike.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.math.BigDecimal;
-import java.util.Date;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
